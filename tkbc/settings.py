61efbf6c832e4d4da4ecd4f44e4b620cea3cee45
Joetlobb <joetlobb@gmail.com>
2020-08-12 21:22:09 +0700

    Setup lising URLs, templates, postgres and pgadmin setup, schemas, create models for lisings and realtors and migrations,

--- tkbc/settings.py
+++ tkbc/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +79,11 @@ WSGI_APPLICATION = 'tkbc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tkbcdb',
+        'USER': 'postgres',
+        'PASSWORD': 'afvb1234',
+        'HOST': 'localhost'
     }
 }
 
