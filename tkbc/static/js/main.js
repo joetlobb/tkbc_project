f8a08218dfb36ad3292aaa75a1d053223cfa443d
Joetlobb <joetlobb@gmail.com>
2020-08-18 07:54:39 +0700

    Fix bg

--- tkbc/static/js/main.js
+++ tkbc/static/js/main.js
@@ -4,3 +4,10 @@ document.querySelector('.year').innerHTML = date.getFullYear();
 $(".alert-dismissible").fadeTo(2000, 700).slideUp(700, function(){
     $(".alert-dismissible").alert('close');
 });
+
+$(window).scroll(function() {
+    var scrolledY = $(window).scrollTop();
+    $('#container').css('background-position', 'left ' + ((scrolledY)) + 'px');
+  });
+  
+  
